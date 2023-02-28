c48a557320250c8f99fcb2e98e6d67e8ab979777
Paisit Jarunnamsiri <paisitj@MacBook-Pro.local>
2023-02-28 15:18:58 -0800

    ch7 - reject invalid subscribers #2

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-use sqlx::postgres::PgPoolOptions;
-use std::net::TcpListener;
-
 use zero2prod::configuration::get_configuration;
-use zero2prod::startup::run;
+use zero2prod::startup::Application;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
@@ -12,13 +9,8 @@ async fn main() -> std::io::Result<()> {
 
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
-    // We have removed the hard-coded `8000` - it's now coming from our settings!
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await?;
+    let application = Application::build(configuration).await?;
+    application.run_until_stopped().await?;
+
     Ok(())
 }
